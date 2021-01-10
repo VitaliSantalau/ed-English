85582119ebe90fc3e50aa2f822f2ef49c3c8e44d
VitaliSantalau <v.santalau@gmail.com>
2021-01-10 23:48:41 +0300

    deploy to vercel start

---
+++ components/header.js
@@ -0,0 +1,14 @@
+import style from './header.module.css'
+
+export default function Header() {
+  return (
+    <header className={style.header}>
+      <div>
+        sing in
+      </div>
+      <div>
+        sing up
+      </div>
+    </header>
+  )
+}
