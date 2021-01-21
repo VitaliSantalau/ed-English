7f53a550d4da70fb04771f381d318a019a8bfcd2
VitaliSantalau <v.santalau@gmail.com>
2021-01-21 16:23:11 +0300

    change base_url

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   env: {
-    "BASE_URL": "http://localhost:3000",
+    /*"BASE_URL": "http://localhost:3000",*/
+    "BASE_URL": "https://ed-english.vercel.app",
     "MONGODB_URL": "mongodb+srv://VitaliSantalau:CyrZLqhnd61eWgR6@cluster0.efytr.mongodb.net/ed-database?retryWrites=true&w=majority",
     "ACCESS_TOKEN_SECRET": "AfeR{gykf[Db(8$xe`5v#&8P/rW_^ZccCzA-kaP7M2UGFq{<",
     "REFRESH_TOKEN_SECRET": "@w2s@@kKh;fq/@z!#p/yB%3a9xf+rK'25{U]9!Q(HK#=YjeVkH!^Abv<<s&KJ'fw;*ms}^*~jy"
