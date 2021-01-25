d635e3d162628c0a06fc59c7993308922469e859
VitaliSantalau <v.santalau@gmail.com>
2021-01-25 17:36:48 +0300

    fff

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,64 @@
 import { connectToMongodb } from '../utils/connectToMongodb'
 import Layout from '../components/layout'
 
-export default function Home({ users }) {
-  
 
+export default function Home({ users }) {
+
+/*const res = await postData('auth/register', userData) 
+
+
+  export const postData = async (url='auth/register', userData) => {
+    const res = await fetch(`${baseUrl}/api/${url}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(userData)
+    })
   
+    const data = await res.json()
+    return data
+  }
+ 
+connectToMongodb ()
+
+  const register = async (req, res) => {
+    try {
+      const { name} = req.body
+
+      const newUser = new Users({
+        name
+      })
+      
+      await newUser.save()
+      
+  
+    } catch (error) {
+        return res.status(500).json({err: err.message})
+    }
+  }*/
+
+
+
+
   return (
     <Layout>
       <main>
       <div>Hi</div>
+     
       {users.map((user) => (
         <div>
         <div>{user.name}</div>
         <div>{user.email}</div>
         </div>
       ))}
-      <button>click</button>
+      <button >click</button>
       </main>
     </Layout>
   )
 }
+
+
 
       
 export async function getServerSideProps(context) {
@@ -30,7 +69,7 @@ export async function getServerSideProps(context) {
 
   return {
     props: {
-      users: JSON.parse(JSON.stringify(users)),
+      users: JSON.parse(JSON.stringify(users))
     },
   };
 }
