c7144bd05ffb728b8013beca20e2b21173cd2053
VitaliSantalau <v.santalau@gmail.com>
2021-01-27 01:12:25 +0300

    next

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -4,10 +4,14 @@ import Providers from 'next-auth/providers'
 const options = {
   // Configure one or more authentication providers
   providers: [
-    Providers.GitHub({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET
+    Providers.Email({
+      server: process.env.EMAIL_SERVER, 
+      from: process.env.EMAIL_FROM,
     }),
+    Providers.Google({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET
+    })
     // ...add more providers here
   ],
 
