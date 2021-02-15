04f963f92e2c1e19559c72b8d960176d82e0ed05
VitaliSantalau <v.santalau@gmail.com>
2021-02-16 00:03:51 +0300

    next

--- components/feedbackForm.js
+++ components/feedbackForm.js
@@ -1,52 +1,42 @@
 import style from './feedbackForm.module.css'
-import React, { useEffect, useState } from 'react'
+import React, { useReducer, useState } from 'react'
 import emailjs, { init } from 'emailjs-com'
 init("user_7CINYP2z8ICW8nhDeIgnc");
 
-const initState = {
-  senderEmail : "",
-  feedback : "",
-  isShowFormFeedback: true,
-  isShowLoading: false,
-  isShowSuccessMessage: false,
-  isShowErrorMessage: false,
-}
+const errorMessage =
+<div className={style.errorMessage}>
+  <div>SORRY ... </div>
+  <div>Your message hasn't been sent</div>
+</div>
+
+const successMessage =
+<div className={style.successMessage}>
+  <div>SUCCESS</div>
+  <div>Your message has been sent</div>
+</div>
 
 function reducer(state, action) {
   switch (action.type) {
-    case 'input user answer':
+    case 'success':
       return {
         ...state, 
-        userAnswer : action.payload
+        view : successMessage
       }
-    case 'correct':
+    case 'error':
       return {
         ...state, 
-        result : 
-          <div className={style.containerCorrect}>
-            <p>correct</p>
-          </div>
+        view : errorMessage
       }
-    case 'correct with help':
+    case 'loading':
       return {
         ...state, 
-        result : 
-          <div className={style.containerCorrect}>
-            <p>correct with help</p>
-          </div>
-      }
-    case 'wrong':
+        isShowLoading: true
+      }  
+    case 'reset':
       return {
-        ...state, 
-        result : 
-          <div className={style.containerWrong}>
-            <p>wrong</p>
-          </div>
-      }
-    case 'open shield':
-      return {
-        ...state, 
-        classShield: style.shieldHelpWordsOpen
+        ...state,
+        view: null, 
+        isShowLoading: false
       }  
     default:
       throw new Error();
@@ -55,97 +45,83 @@ function reducer(state, action) {
 
 export default function FeedbackForm({ handleShowFeedBack }) {
 
-  /*const [state, dispatch] = useReducer(reducer, initialState) */
-  
-  /*
-  const handleChangeInput = e => {
-    dispatch({
-      type: "input user answer",
-      payload: e.target.value
-    })
-  }
-  */
-
-  const [state, setState] = useState(initState);
-  
-  const { senderEmail, feedback, isShowLoading, isShowSuccessMessage, isShowErrorMessage, isShowFormFeedback } = state
+  const [input, setInput] = useState({ senderEmail: "", feedback: "" });
+  const { senderEmail, feedback } = input
 
   const handleInput = (e) => {
-    setState({
-      ...state,
+    setInput({
+      ...input,
       [e.target.name] : e.target.value
     })
   }
 
+
+const initialState = {
+  view: null,
+  isShowLoading: false,
+} 
+
+  const [state, dispatch] = useReducer(reducer, initialState)
+  
+  
   const handleSend = () => {
-    setState({
-      ...state,
-      isLoading: true
-    })
-    emailjs.send("service_ntqo2if", "template_39g6d8t", state)
+    dispatch({ type: 'loading' })
+    emailjs.send("service_ntqo2if", "template_39g6d8t", input)
     .then(response => {
-      setState({
-        ...state,
-        isShowFormFeedback: false,
-        isShowSuccessMessage: true,
-      })
+      dispatch({ type: 'success' })
       setTimeout(handleShowFeedBack, 3000)
+
       console.log('SUCCESS!', response.status, response.text);
-      setTimeout(setState(initState), 3000)
+      
     }, function(error) {
+      dispatch({ type: 'error' })
+      setTimeout(handleShowFeedBack, 3000)
+      /*setTimeout(dispatch({ type: 'reset' }), 10000)      
+      /*setTimeout(setInput({ senderEmail: "", feedback: "" }), 4000)*/
       console.log('FAILED...', error);
     });
   }
 
   
   return (
-    <>
-      {isShowFormFeedback && 
-      <div className={style.form}>
-        <label className={style.email}>
-          Email
-          <input 
-            type="text" 
-            className={style.inputEmail}
-            autoFocus
-            placeholder="Your email address..."
-            name="senderEmail"
-            value={senderEmail}
-            onChange={handleInput}
-          />
-        </label>
-        <label className={style.feedback}>
-          Feedback
-          <textarea 
-            className={style.inputFeedback}
-            placeholder="Your feedback..."
-            name="feedback"
-            value={feedback}
-            onChange={handleInput}
-          />
-        </label>
-        <div className={style.containerButtons}>
-          {isShowLoading ? <button className={style.buttonLoading}>
-            Loading...
-          </button> :
-          <button className={style.buttonSend} onClick={handleSend}>
+  <>
+    <div className={style.form}>
+      <label className={style.email}>
+        Email
+        <input 
+          type="text" 
+          className={style.inputEmail}
+          autoFocus
+          placeholder="Your email address..."
+          name="senderEmail"
+          value={senderEmail}
+          onChange={handleInput}
+        />
+      </label>
+      <label className={style.feedback}>
+        Feedback
+        <textarea 
+          className={style.inputFeedback}
+          placeholder="Your feedback..."
+          name="feedback"
+          value={feedback}
+          onChange={handleInput}
+        />
+      </label>
+      <div className={style.containerButtons}>
+        {state.isShowLoading ? <button className={style.buttonLoading}>
+          Loading...
+        </button> :
+        <button className={style.buttonSend} onClick={handleSend}>
           Send
-        </button>}         
-          <button onClick={handleShowFeedBack} className={style.buttonClose}>
-            Close
-          </button>
-        </div>
-      </div>} 
-      {isShowSuccessMessage &&
-      <div className={style.successMessage}>
-        <div>SUCCESS</div>
-        <div>Your message has been sent</div>
-      </div>}
-      {isShowErrorMessage &&
-      <div className={style.errorMessage}>
-        <div>SORRY ... </div>
-        <div>Your message hasn't been sent</div>
-      </div>}
-    </>
+  </button>}         
+        <button onClick={handleShowFeedBack} className={style.buttonClose}>
+          Close
+        </button>
+      </div>
+    </div>
+    {state.view}
+  </>
   )
+
 }
